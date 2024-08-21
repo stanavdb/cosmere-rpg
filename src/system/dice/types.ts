e457e700cbaf5cf11df5acc30d2e542dd4bad0b2
Tobias Berger <toby@tobot.dev>
2024-08-21 13:35:24 +0200

    chore(tying): fix types and lint

--- src/system/dice/types.ts
+++ src/system/dice/types.ts
@@ -1,6 +1,1 @@
-export interface RollToMessageOptions {
-    rollMode?: keyof CONFIG.Dice.RollModes | 'roll';
-    create?: boolean;
-}
-
 export type RollMode = keyof CONFIG.Dice.RollModes | 'roll';
