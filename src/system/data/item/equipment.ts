a09941016ff2d083997d60c65fed9245f777153a
stanavdb <stanvdbroekk@gmail.com>
2024-09-24 13:52:23 +0200

    feat(data models): expand equipment data model

--- src/system/data/item/equipment.ts
+++ src/system/data/item/equipment.ts
@@ -1,3 +1,6 @@
+import { EquipmentType } from '@system/types/cosmere';
+import { CosmereItem } from '@src/system/documents';
+
 // Mixins
 import { DataModelMixin } from '../mixins';
 import { TypedItemMixin, TypedItemData } from './mixins/typed';
@@ -6,16 +9,28 @@ import {
     DescriptionItemData,
 } from './mixins/description';
 import { PhysicalItemMixin, PhysicalItemData } from './mixins/physical';
+import {
+    ActivatableItemMixin,
+    ActivatableItemData,
+} from './mixins/activatable';
 
 export interface EquipmentItemData
-    extends TypedItemData,
+    extends TypedItemData<EquipmentType>,
         DescriptionItemData,
-        PhysicalItemData {}
+        PhysicalItemData,
+        ActivatableItemData {}
 
-export class EquipmentItemDataModel extends DataModelMixin(
-    TypedItemMixin(),
+export class EquipmentItemDataModel extends DataModelMixin<
+    EquipmentItemData,
+    CosmereItem
+>(
+    TypedItemMixin({
+        initial: EquipmentType.Basic,
+        choices: () => Object.keys(CONFIG.COSMERE.items.equipment.types),
+    }),
     DescriptionItemMixin(),
     PhysicalItemMixin(),
+    ActivatableItemMixin(),
 ) {
     static defineSchema() {
         return foundry.utils.mergeObject(super.defineSchema(), {});
