9b885fb2d8059f8298f84f43c66a8f3fcae096eb
stanavdb <stanvdbroekk@gmail.com>
2024-09-27 23:21:53 +0200

    feat(item sheets): make document name editable

--- src/system/applications/item/components/header.ts
+++ src/system/applications/item/components/header.ts
@@ -10,6 +10,49 @@ export class ItemHeaderComponent extends HandlebarsApplicationComponent<
     static TEMPLATE =
         'systems/cosmere-rpg/templates/item/components/header.hbs';
 
+    /**
+     * NOTE: Unbound methods is the standard for defining actions
+     * within ApplicationV2
+     */
+    /* eslint-disable @typescript-eslint/unbound-method */
+    static ACTIONS = {
+        'edit-name': this.onEditName,
+    };
+    /* eslint-enable @typescript-eslint/unbound-method */
+
+    /* --- Actions --- */
+
+    private static onEditName(this: ItemHeaderComponent) {
+        // Hide name span
+        $(this.element!).find('.document-name span').hide();
+
+        // Find input element
+        const input = $(this.element!).find('.document-name input');
+
+        // Check if input has focus
+        if (input.is(':focus')) return;
+
+        // Show input element
+        input.show();
+
+        setTimeout(() => {
+            // Select the text
+            input.trigger('select');
+
+            // Add blur handler
+            input.on('blur', () => {
+                // Remove handler
+                input.off('blur');
+
+                // Hide input element
+                input.hide();
+
+                // Show name span
+                $(this.element!).find('.document-name span').show();
+            });
+        });
+    }
+
     /* --- Context --- */
 
     public _prepareContext(params: never, context: BaseItemSheetRenderContext) {
