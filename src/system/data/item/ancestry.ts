41f14c2de3a4c9b1ab9cf61c83da2ccc4b066eb9
Zithith <jonnyt.jet@gmail.com>
2024-10-06 14:57:36 +0100

    feat(item sheets): Ancestry details tab placeholders replaced with valid controls.

--- src/system/data/item/ancestry.ts
+++ src/system/data/item/ancestry.ts
@@ -9,12 +9,30 @@ import {
     DescriptionItemData,
 } from './mixins/description';
 
+interface TalentGrant {
+    id: string;
+    name: string;
+    level: number;
+}
+
+interface ExtraTalentPicks {
+    restrictions: string; // TODO: link up with the Talent Pre-reqs?;
+    levels: {
+        level: number;
+        quantity: number;
+    }[];
+}
 export interface AncestryItemData extends IdItemData, DescriptionItemData {
     size: Size;
     type: {
         id: CreatureType;
         custom?: string | null;
         subtype?: string | null;
+    };
+    advancement: {
+        extraPath: string;
+        extraTalents: TalentGrant[];
+        extraTalentPicks: ExtraTalentPicks;
     };
 }
 
@@ -36,7 +54,13 @@ export class AncestryItemDataModel extends DataModelMixin<
                 nullable: false,
                 blank: false,
                 initial: Size.Medium,
-                choices: Object.keys(CONFIG.COSMERE.sizes),
+                choices: Object.entries(CONFIG.COSMERE.sizes).reduce(
+                    (acc, [key, config]) => ({
+                        ...acc,
+                        [key]: config.label,
+                    }),
+                    {},
+                ),
             }),
             type: new foundry.data.fields.SchemaField({
                 id: new foundry.data.fields.StringField({
@@ -44,7 +68,15 @@ export class AncestryItemDataModel extends DataModelMixin<
                     nullable: false,
                     blank: false,
                     initial: CreatureType.Humanoid,
-                    choices: Object.keys(CONFIG.COSMERE.creatureTypes),
+                    choices: Object.entries(
+                        CONFIG.COSMERE.creatureTypes,
+                    ).reduce(
+                        (acc, [key, config]) => ({
+                            ...acc,
+                            [key]: config.label,
+                        }),
+                        {},
+                    ),
                 }),
                 custom: new foundry.data.fields.StringField({ nullable: true }),
                 subtype: new foundry.data.fields.StringField({
@@ -54,14 +86,17 @@ export class AncestryItemDataModel extends DataModelMixin<
             advancement: new foundry.data.fields.SchemaField({
                 extraTalentPicks: new foundry.data.fields.SchemaField({
                     levels: new foundry.data.fields.ArrayField(
-                        new foundry.data.fields.NumberField(),
+                        new foundry.data.fields.SchemaField({
+                            level: new foundry.data.fields.NumberField(),
+                            quantity: new foundry.data.fields.NumberField(),
+                        }),
                     ),
-                    restrictions: new foundry.data.fields.ObjectField(),
-                    // ^ how to define a rule object?... e.g. "only attaches to owned talent in singer tree"
+                    restrictions: new foundry.data.fields.StringField(),
                 }),
-                extraTalentTree: new foundry.data.fields.StringField(),
+                extraPath: new foundry.data.fields.StringField(),
                 extraTalents: new foundry.data.fields.ArrayField(
                     new foundry.data.fields.SchemaField({
+                        id: new foundry.data.fields.StringField(),
                         name: new foundry.data.fields.StringField(),
                         level: new foundry.data.fields.NumberField(),
                     }),
@@ -78,5 +113,13 @@ export class AncestryItemDataModel extends DataModelMixin<
 
     get sizeField(): foundry.data.fields.StringField {
         return this.schema.fields.size as foundry.data.fields.StringField;
+    }
+
+    get extraTalents(): TalentGrant[] {
+        return this.advancement.extraTalents;
+    }
+
+    get extraTalentPicks(): ExtraTalentPicks {
+        return this.advancement.extraTalentPicks;
     }
 }
