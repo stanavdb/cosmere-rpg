6d455f6573b115c07c8a10b23f189e3132c171a0
stanavdb <stanvdbroekk@gmail.com>
2024-10-20 10:23:41 +0200

    chore(data models): improve talent item validation error messages

--- src/system/data/item/talent.ts
+++ src/system/data/item/talent.ts
@@ -207,31 +207,48 @@ export class TalentItemDataModel extends DataModelMixin<
                         nullable: true,
                         validate: (value?: Partial<Talent.Prerequisite>) => {
                             if (!value) return;
-
                             switch (value.type) {
                                 case Talent.Prerequisite.Type.Talent:
-                                    return (
-                                        !!value.talents &&
-                                        value.talents.length > 0 &&
-                                        !!value.mode
-                                    );
+                                    if (!value.talents)
+                                        throw new Error(
+                                            'Field "talents" is required for prerequisite rule of type "Talent"',
+                                        );
+                                    break;
                                 case Talent.Prerequisite.Type.Attribute:
-                                    return (
-                                        !!value.attribute &&
-                                        value.attribute.length > 0 &&
-                                        !!value.value
-                                    );
+                                    if (
+                                        !value.attribute ||
+                                        value.attribute.length === 0
+                                    )
+                                        throw new Error(
+                                            'Field "attribute" is required for prerequisite rule of type "Attribute"',
+                                        );
+                                    if (!value.value)
+                                        throw new Error(
+                                            'Field "value" is required for prerequisite rule of type "Attribute"',
+                                        );
+                                    break;
                                 case Talent.Prerequisite.Type.Skill:
-                                    return (
-                                        !!value.skill &&
-                                        value.skill.length > 0 &&
-                                        !!value.rank
-                                    );
+                                    if (
+                                        !value.skill ||
+                                        value.skill.length === 0
+                                    )
+                                        throw new Error(
+                                            'Field "skill" is required for prerequisite rule of type "Skill"',
+                                        );
+                                    if (!value.rank)
+                                        throw new Error(
+                                            'Field "rank" is required for prerequisite rule of type "Skill"',
+                                        );
+                                    break;
                                 case Talent.Prerequisite.Type.Connection:
-                                    return (
-                                        !!value.description &&
-                                        value.description.length > 0
-                                    );
+                                    if (
+                                        !value.description ||
+                                        value.description.length === 0
+                                    )
+                                        throw new Error(
+                                            'Field "description" is required for prerequisite rule of type "Connection"',
+                                        );
+                                    break;
                                 default:
                                     return false;
                             }
