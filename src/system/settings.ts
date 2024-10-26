5abe33bd7ab97d41dc3b3fcb60734838814b79b0
Mangó <mango.shadowplay@gmail.com>
2024-10-26 20:02:36 +0200

    Add keybindings setup

--- src/system/settings.ts
+++ src/system/settings.ts
@@ -64,10 +64,60 @@ export function registerSystemSettings() {
 }
 
 /**
+ * Index of identifiers for system keybindings.
+ */
+export const KEYBINDINGS = {
+    SKIP_DIALOG_DEFAULT: 'skipDialogDefault',
+    SKIP_DIALOG_ADVANTAGE: 'skipDialogAdvantage',
+    SKIP_DIALOG_DISADVANTAGE: 'skipDialogDisadvantage',
+    SKIP_DIALOG_RAISE_STAKES: 'skipDialogRaiseStakes',
+};
+
+/**
+ * Register all of the system's keybindings.
+ */
+export function registerSystemKeybindings() {
+    const keybindings = [
+        {
+            name: KEYBINDINGS.SKIP_DIALOG_DEFAULT,
+            editable: [{ key: 'AltLeft' }, { key: 'AltRight' }],
+        },
+        {
+            name: KEYBINDINGS.SKIP_DIALOG_ADVANTAGE,
+            editable: [{ key: 'ShiftLeft' }, { key: 'ShiftRight' }],
+        },
+        {
+            name: KEYBINDINGS.SKIP_DIALOG_DISADVANTAGE,
+            editable: [{ key: 'ControlLeft' }, { key: 'ControlRight' }],
+        },
+        {
+            name: KEYBINDINGS.SKIP_DIALOG_RAISE_STAKES,
+            editable: [{ key: 'MetaLeft' }, { key: 'MetaRight' }],
+        },
+    ];
+
+    keybindings.forEach((keybind) => {
+        game.keybindings!.register(SYSTEM_ID, keybind.name, {
+            name: `KEYBINDINGS.${keybind.name}`,
+            editable: keybind.editable,
+        });
+    });
+}
+
+/**
  * Retrieve a specific setting value for the provided key.
  * @param {string} settingKey The identifier of the setting to retrieve.
  * @returns {string|boolean} The value of the setting as set for the world/client.
  */
 export function getSystemSetting(settingKey: string) {
     return game.settings!.get(SYSTEM_ID, settingKey);
+}
+
+/**
+ * Retrieves an array of keybinding values for the provided key.
+ * @param {string} keybindingKey The identifier of the keybinding to retrieve.
+ * @returns {Array<object>} The value of the keybindings associated with the given key.
+ */
+export function getSystemKeybinding(keybindingKey: string) {
+    return game.keybindings!.get(SYSTEM_ID, keybindingKey);
 }
