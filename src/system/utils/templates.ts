c8373eabd350897cde02cbed1ac48ea81261eebe
Mangó <mango.shadowplay@gmail.com>
2024-11-06 16:19:09 +0100

    Move all templates to unified const

--- src/system/utils/templates.ts
+++ src/system/utils/templates.ts
@@ -4,6 +4,44 @@ import { SYSTEM_ID } from '../constants';
  * Index of identifiers for system templates.
  */
 export const TEMPLATES = {
+    GENERAL_TABS: 'general/tabs.hbs',
+    COMBAT_COMBATANT: 'combat/combatant.hbs',
+
+    // ACTOR
+    ACTOR_CHARACTER_DETAILS_TAB:
+        'actors/character/partials/char-details-tab.hbs',
+    ACTOR_CHARACTER_ACTIONS_TAB:
+        'actors/character/partials/char-actions-tab.hbs',
+    ACTOR_CHARACTER_EQUIPMENT_TAB:
+        'actors/character/partials/char-equipment-tab.hbs',
+    ACTOR_CHARACTER_GOALS_TAB: 'actors/character/partials/char-goals-tab.hbs',
+    ACTOR_CHARACTER_EFFECTS_TAB:
+        'actors/character/partials/char-effects-tab.hbs',
+    ACTOR_ADVERSARY_ACTIONS_TAB:
+        'actors/adversary/partials/adv-actions-tab.hbs',
+    ACTOR_ADVERSARY_EQUIPMENT_TAB:
+        'actors/adversary/partials/adv-equipment-tab.hbs',
+    ACTOR_ADVERSARY_EFFECTS_TAB:
+        'actors/adversary/partials/adv-effects-tab.hbs',
+
+    //ITEM
+    ITEM_DESCRIPTION_TAB: 'item/partials/item-description-tab.hbs',
+    ITEM_EFFECTS_TAB: 'item/partials/item-effects-tab.hbs',
+    ITEM_DETAILS_TAB: 'item/partials/item-details-tab.hbs',
+    ITEM_INJURY_DETAILS_TAB: 'item/injury/partials/injury-details-tab.hbs',
+    ITEM_SPECIALTY_DETAILS_TAB:
+        'item/specialty/partials/specialty-details-tab.hbs',
+    ITEM_LOOT_DETAILS_TAB: 'item/loot/partials/loot-details-tab.hbs',
+    ITEM_ARMOR_DETAILS_TAB: 'item/armor/partials/armor-details-tab.hbs',
+    ITEM_ANCESTRY_DETAILS_TAB:
+        'item/ancestry/partials/ancestry-details-tab.hbs',
+    ITEM_TALENT_DETAILS_TAB: 'item/talent/partials/talent-details-tab.hbs',
+    ITEM_ACTION_DETAILS_TAB: 'item/action/partials/action-details-tab.hbs',
+    ITEM_GOAL_DETAILS_TAB: 'item/goal/partials/goal-details-tab.hbs',
+    ITEM_POWER_DETAILS_TAB: 'item/power/partials/power-details-tab.hbs',
+    ITEM_PATH_DETAILS_TAB: 'item/path/partials/path-details-tab.hbs',
+
+    //CHAT
     CHAT_CARD_HEADER: 'chat/card-header.hbs',
     CHAT_CARD_CONTENT: 'chat/card-content.hbs',
     CHAT_CARD_SECTION: 'chat/card-section.hbs',
@@ -16,9 +54,6 @@ export const TEMPLATES = {
 
     CHAT_OVERLAY_D20: 'chat/overlay-d20.hbs',
     CHAT_OVERLAY_CRIT: 'chat/overlay-crit.hbs',
-
-    CHAT_CARD_ROLLS: 'chat/parts/chat-card-rolls.hbs',
-    CHAT_CARD_ACTIONS: 'chat/parts/chat-card-actions.hbs',
 } as const;
 
 /**
